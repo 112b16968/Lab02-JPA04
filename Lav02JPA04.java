1566bcf8d2f1b72dbf876ccc89e50f8c3355b114
112b16968 <160818741+112b16968@users.noreply.github.com>
2024-03-05 09:54:21 +0800

    Add files via upload

--- Lav02JPA04.java
+++ Lav02JPA04.java
@@ -2,18 +2,29 @@ import java.util.Scanner;
 
 public class Lav02JPA04 {
     public static void main(String[] args) {
-        for (int i =0 ; i<2 ; i++){ 
-        System.out.println("Input:");
-        Scanner input1 = new Scanner(System.in);
-        int number1 = input1.nextInt();
-        if (number1 % 5 == 0 & number1 % 9==0) {
-            System.out.println("Yes");
-            
-        }else{
-            System.out.println("No");
+        // 建立一個 Scanner 物件，迴圈外部使用
+        Scanner scanner = new Scanner(System.in);
 
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Input:");
+
+            if (scanner.hasNextInt()) {
+                int number1 = scanner.nextInt();
+
+                if (number1 % 5 == 0 && number1 % 9 == 0) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter an integer.");
+                // 清空 scanner 的緩衝區
+                scanner.next();
+            }
         }
 
-    }  
+        // 關閉 Scanner
+        scanner.close();
     }
 }
+
